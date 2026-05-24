3f73354441349deb41684926cda62f6c8744e2f9
bottlemoth <Jacek.Kuczak1@gmail.com>
2026-05-24 16:27:35 +0200

    konteneryzacja frontendu

--- backend/wspolpracujmy/Program.cs
+++ backend/wspolpracujmy/Program.cs
@@ -20,7 +20,9 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "http://localhost:3000",   
+                "http://127.0.0.1:3000"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
